5c9f2f1f0398bc066d556abd87185b2e92afbbf8
MorBraSto79 <s326152@oslomet.no>
2020-10-21 10:34:45 +0200

    Oppgave 1.1.3-6

--- src/Section1_1_3/Number6.java
+++ src/Section1_1_3/Number6.java
@@ -1,4 +1,23 @@
 package Section1_1_3;
 
 public class Number6 {
+    //Utrykket n! betyr n fakultet og er gitt ved n! = n · (n-1) ·  ·  · 2 · 1 . Lag en metode
+    //long fak(int n) som regner ut n!. Hvor mange multiplikasjoner utføres i metoden?
+
+    public static long fak(int n){
+        long fak = 1;
+
+        for(int i = 1; i <= n; i++){
+            fak = i * fak;
+        }
+
+        return fak;
+
+        //Metoden utfører n-1 multiplikasjoner
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(fak(5));
+    }
 }
